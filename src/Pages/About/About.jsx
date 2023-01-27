dedd3b4e790da2ad51f9851430d7e5aade716c0e
= <iarlensr@hotmail.com>
2023-01-27 16:18:34 -0300

    Adicionando conteúdo da pagina about

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 
-import { Main } from "./styles";
+import { Main, HeaderStyled, ImageContainerStyled } from "./styles";
+import aboutImage from "/home/iarlen/Documentos/GithubRepository/public/about-image.png";
+
 const About = () => {
   return (
     <div>
       <Main>
-        <h1>Rota de sobre</h1>
+        <HeaderStyled>
+          <h1>FindRepository</h1>
+          <p>
+            Essa aplicação foi desenvolvida utilizando a
+            <a href="https://api.github.com/"> API</a> do próprio github.
+          </p>
+          <p>
+            O intuito da criação do projeto foi praticar os conceitos de
+            componentização do reactJs e requisição com axios, junto ao reactJs.
+          </p>
+        </HeaderStyled>
+        <ImageContainerStyled>
+          <img
+            src={aboutImage}
+            alt="Uma mulher ao lado de um computador, tablet e celular"
+          />
+        </ImageContainerStyled>
       </Main>
     </div>
   );
--- src/Pages/About/styles.js
+++ src/Pages/About/styles.js
@@ -3,5 +3,64 @@ import styled from "styled-components";
 export const Main = styled.main`
   display: flex;
   align-items: center;
-  justify-content: center;
+  flex-direction: column;
+
+  height: 100vh;
+
+  padding: 1rem;
+
+  row-gap: 2rem;
+`;
+
+export const HeaderStyled = styled.header`
+  display: flex;
+  flex-direction: column;
+
+  text-align: center;
+  align-items: center;
+  row-gap: 0.3rem;
+
+  h1 {
+    color: #fff;
+
+    font-style: italic;
+
+    margin-top: 1rem;
+    margin-bottom: 0.5rem;
+  }
+
+  p {
+    max-width: 600px;
+
+    font-style: italic;
+    font-size: 1.2rem;
+    font-weight: 500;
+
+    color: #aaa;
+  }
+
+  p a {
+    color: #000;
+    transition: 0.4s;
+
+    &:hover {
+      color: crimson;
+    }
+  }
+`;
+
+export const ImageContainerStyled = styled.div`
+  max-width: 500px;
+  margin: 0 auto;
+
+  img {
+    max-width: 100%;
+    transition: 0.6s;
+
+    cursor: pointer;
+  }
+
+  img:hover {
+    transform: scale(1.1);
+  }
 `;
